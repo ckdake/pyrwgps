02bc8f4be2c94ab9702e918f9e4bd2330a09e67c
Chris Kelly <ckdake@ckdake.com>
2025-06-07 03:24:27 +0000

    Sets workflow permissions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ridewithgps",
-    version="0.0.2",
+    version="0.0.3",
     description="Python client for the RideWithGPS API",
     author="Chris Kelly",
     packages=["ridewithgps"],
